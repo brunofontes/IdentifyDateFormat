6070b3079626c7f35fb0e8c331da3314e3c9c174
Bruno Fontes <developer@brunofontes.net>
2023-03-24 22:47:35 -0300

    test: adding tests

--- identifydateformat_test.go
+++ identifydateformat_test.go
@@ -1,7 +1,7 @@
 package identifydateformat
 
 import (
-	"fmt"
+	"reflect"
 	"testing"
 )
 
@@ -9,6 +9,35 @@ import (
 // MONTH: 01
 // DAY: 02
 
-func TestOpenCsv(t *testing.T) {
-	fmt.Printf("\n%#v\n", identifydateformat([]string{"02/02/02", "23/12/01", "10/12/32", "01/10/01"}))
+// TestIdentifyDateFormat returns the only format that matches the given dates
+func TestIdentifyDateFormat(t *testing.T) {
+	expect := []string{"02/01/06"}
+	got, err := IdentifyDateFormat([]string{"02/02/02", "23/12/01", "10/12/32", "01/10/01"})
+	if err != nil {
+		t.Errorf("Expected '%#v', got nil\n", expect)
+	}
+	if !reflect.DeepEqual(expect, got) {
+		t.Errorf("Expected '%#v', got '%#v'\n", expect, got)
+	}
+}
+
+// TestErrorIdentifyingDateFormat should return error as there is no single
+// format that could matches the given dates
+func TestErrorIdentifyingDateFormat(t *testing.T) {
+	got, err := IdentifyDateFormat([]string{"30/03/1980", "03/30/1980", "1980-03-30"})
+	if err == nil {
+		t.Errorf("Expected error, got '%#v'\n", got)
+	}
+}
+
+// TestAddPossibleFormats do not add any formats that already exists
+func TestAddPossibleFormats(t *testing.T) {
+	DateFormats = []string{"a", "b", "c"}
+	AddPossibleFormats("b", "a", "d", "a", "d")
+
+	expect := []string{"a", "b", "c", "d"}
+	got := DateFormats
+	if !reflect.DeepEqual(expect, got) {
+		t.Errorf("Expected '%#v', got '%#v'\n", expect, got)
+	}
 }
